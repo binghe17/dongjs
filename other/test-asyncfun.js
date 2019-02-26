ef2b67028ef50ed2ad587c0eca2558844ecb4a68
zun <binghe17@qq.com>
2019-02-26 21:58:42 +0900

    Add files via upload

---
+++ other/test-asyncfun.js
@@ -0,0 +1,31 @@
+let {log} = console
+
+
+async function asyncFn(){
+	log('start')
+	let v
+	v = await new Promise((y,n)=>{
+		setTimeout(()=>{
+			y('结果A')
+		},1000)
+	})
+
+	console.log(1, v)
+	console.log(2, v)
+	
+	
+	let v2 = await new Promise((y,n)=>{
+		setTimeout(()=>{
+			y('结果B')
+		}, 500)
+	})
+	log(3, v2)
+	log('end')
+
+	return {v, v2}
+}
+
+
+
+asyncFn().then(v=>log(v))
+
